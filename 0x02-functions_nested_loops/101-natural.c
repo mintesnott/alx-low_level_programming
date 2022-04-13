cbdf27de60dd59c6367351099f1862a9cd960f5c
mintesnot <mintesnotkebede6@gmail.com>
2022-04-13 07:33:33 -0700

    task 13 is done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  * print_times_table - prints the times table of the input, starting with 0.
- *   * @n: the value of the times table to be printed
- *    */
+ * print_times_table - prints the times table of the input, starting with 0.
+ *  @n: the value of the times table to be printed
+ */
 void print_times_table(int n)
 {
 int num, mult, prod;
