c0ee34b62e25150f8a3bc3ea6621d3ad0f4b34b5
mintesnot <mintesnotkebede6@gmail.com>
2022-04-14 01:19:04 -0700

    task 13 is done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,45 +1,18 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * print_times_table - prints the times table of the input, starting with 0.
- *  @n: the value of the times table to be printed
+ * main - lists all the natural numbers below 1024
+ *        (excluded) that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
  */
-void print_times_table(int n)
+int main(void)
 {
-int num, mult, prod;
-if (!(n > 15 || n < 0))
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
 {
-for (num = 0; num <= n; num++)
-{
-for (mult = 0; mult <= n; mult++)
-{
-prod = (num * mult);
-if (mult != 0)
-{
-_putchar(',');
-_putchar(' ');
+if ((i % 3) == 0 || (i % 5) == 0)
+sum += i;
 }
-if (prod < 10 && mult != 0)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar((prod % 10) + '0');
-}
-else if (prod >= 10 && prod < 100)
-{
-_putchar(' ');
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
-}
-else if (prod >= 100 && mult != 0)
-{
-_putchar((prod / 100) + '0');
-_putchar((prod / 10) % 10 + '0');
-_putchar((prod % 10) + '0');
-}
-else
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
-}
+printf("%d\n", sum);
+return (0);
 }
